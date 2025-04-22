353b1a26be898afbc165d14d34f89a4d40090fa9
William <woviedo@tpaga.co>
2025-04-21 22:00:21 -0500

    Initial Commit

---
+++ Cosmo Noticias/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cosmo Noticias
+//
+//  Created by William Andrés Oviedo Jiménez on 21/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cosmo Noticias/Cosmo_NoticiasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmo_NoticiasApp.swift
+//  Cosmo Noticias
+//
+//  Created by William Andrés Oviedo Jiménez on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Cosmo_NoticiasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
